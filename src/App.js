08b6427afee03f4fa398ba2ea5e9eb5269824e00
GabrielAlbanez <gabriel.g.albabez@gmail.com>
2023-03-22 21:26:51 -0300

    subir aplicativo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,123 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, button, useEffect } from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Texto titulo="Constelação" />
+      <br />
+      <Counter />
+      <img
+        src="https://www.10wallpaper.com/wallpaper/1366x768/1307/fog_constellations_lilac-Space_Discovery_HD_Wallpaper_1366x768.jpg"
+        alt=""
+        className="imagem"
+      />
     </div>
   );
 }
 
-export default App;
+export function Texto(props) {
+  return (
+    <div>
+      <DivCentralizada
+        conteudo={<h1 style={{ color: "white" }}>{props.titulo}</h1>}
+      />
+    </div>
+  );
+}
+
+export function DivCentralizada(props) {
+  return (
+    <div
+      style={{
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "",
+      }}
+    >
+      <div style={{ textAlign: "center" }}>
+        {props.conteudo}
+        {props.imagenzinha}
+      </div>
+    </div>
+  );
+}
+
+export function Counter(props) {
+  useEffect(() => {
+    console.log("esse componenet foi montado");
+  }, []);
+
+  const [contador, setContador] = useState(0);
+  function Adicionar() {
+    if (contador < lista.length - 1) {
+      setContador(contador + 1);
+    } else {
+      setContador(0)
+    }
+  }
+
+  function Remover() {
+    if (contador <= 0) {
+      setContador(lista.length - 1);
+    } else {
+      setContador(contador - 1);
+    }
+  }
+  function Zerar() {
+    setContador(0);
+  }
+  if (contador > 4) {
+    setContador(0);
+  }
+  if (contador < 0) {
+    setContador(0);
+  }
+  const lista = [
+    "https://images6.alphacoders.com/373/thumb-1920-373654.jpg",
+    "https://images6.alphacoders.com/519/519732.jpg",
+    "https://i.pinimg.com/originals/2b/06/96/2b0696cea2d03483ea4dfca2f81b9c1d.jpg",
+    "https://wallpaper-house.com/data/out/9/wallpaper2you_294619.jpg",
+    "https://cdn.wallpapersafari.com/98/52/B2c8A1.jpg",
+  ];
+
+  useEffect(() => {
+    console.log("esse componenet foi atualizado");
+  }, [contador]);
+
+  const [cor, setCor] = useState("white");
+  function Mudarcor() {
+    if (cor == "white") {
+      setCor("black");
+    }
+  }
+
+  return (
+    <div className="centralizar">
+      <img
+        style={{
+          borderRadius: "10%",
+          boxShadow: "12px 12px 30px 15px #3333ff",
+          border: "8px solid #000000",
+        }}
+        src={lista[contador]}
+        alt="img juju"
+        className="img"
+      />
+
+      <h3> Imagem: {contador + 1} </h3>
+
+      <button className="button1" onClick={Adicionar}>
+        avançar
+      </button>
+
+      <button className="button1" onClick={Remover}>
+        voltar
+      </button>
+
+      <button className="button1" onClick={Zerar}>
+        Zerar
+      </button>
+    </div>
+  );
+}
